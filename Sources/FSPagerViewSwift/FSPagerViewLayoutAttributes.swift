bc521c4aa2b1781706e11c796baf777fc392846a
kimkyuchul <rbcjf122@gmail.com>
2024-12-23 15:52:19 +0900

    fix

--- Sources/FSPagerViewSwift/FSPagerViewLayoutAttributes.swift
+++ Sources/FSPagerViewSwift/FSPagerViewLayoutAttributes.swift
@@ -8,26 +8,24 @@
 
 import UIKit
 
-@MainActor
 open class FSPagerViewLayoutAttributes: UICollectionViewLayoutAttributes {
-    @MainActor
     open var position: CGFloat = 0
-    
-    @MainActor
-    open override func isEqual(_ object: Any?) -> Bool {
+
+    nonisolated open override func isEqual(_ object: Any?) -> Bool {
         guard let object = object as? FSPagerViewLayoutAttributes else {
             return false
         }
-
-        let isEqual = super.isEqual(object) && (self.position == object.position)
+        
+        var isEqual = super.isEqual(object)
+        isEqual = isEqual && (self.position == object.position)
+        
         return isEqual
     }
-    
-    @MainActor
-    open override func copy(with zone: NSZone? = nil) -> Any {
+
+    nonisolated open override func copy(with zone: NSZone? = nil) -> Any {
         let copy = super.copy(with: zone) as! FSPagerViewLayoutAttributes
         copy.position = self.position
         return copy
     }
-    
 }
+
